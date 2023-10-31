bf8326c1c604819a0e3121ef6be4d2ae69fa8448
Elena Dulgheru <elenadulgheru03@gmail.com>
2023-10-31 21:28:32 +0200

    Pushed src/mm/*.c

--- src/mm/malloc.c
+++ src/mm/malloc.c
@@ -3,35 +3,49 @@
 #include <internal/mm/mem_list.h>
 #include <internal/types.h>
 #include <internal/essentials.h>
+#include <internal/syscall.h>
 #include <sys/mman.h>
 #include <string.h>
 #include <stdlib.h>
 
-void *malloc(size_t size)
+void* malloc(size_t size)
 {
-	/* TODO: Implement malloc(). */
+	void *ptr = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+	if (!mem_list_add(ptr, size))
+		return ptr;
+
 	return NULL;
 }
 
 void *calloc(size_t nmemb, size_t size)
 {
-	/* TODO: Implement calloc(). */
+	void *ptr = mmap(NULL, size * nmemb, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+	if (!mem_list_add(ptr, size * nmemb))
+		return ptr;
+
+	memset(ptr, 0, size * nmemb);
+
 	return NULL;
 }
 
 void free(void *ptr)
 {
-	/* TODO: Implement free(). */
+	size_t len = mem_list_find(ptr)->len;
+	mem_list_del(ptr);
+	munmap(ptr, len);
 }
 
 void *realloc(void *ptr, size_t size)
 {
-	/* TODO: Implement realloc(). */
-	return NULL;
+	void *new_ptr = mmap(NULL, sizeof(void *), PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+	struct mem_list *old = mem_list_find(ptr);
+
+	syscall(__NR_mremap, ptr, old->len, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, new_ptr);
+
+	return new_ptr;
 }
 
 void *reallocarray(void *ptr, size_t nmemb, size_t size)
 {
-	/* TODO: Implement reallocarray(). */
-	return NULL;
+	return realloc(ptr, size * nmemb);
 }
--- src/mm/mmap.c
+++ src/mm/mmap.c
@@ -7,17 +7,32 @@
 void *mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset)
 {
 	/* TODO: Implement mmap(). */
-	return MAP_FAILED;
+	long ret = syscall(__NR_mmap, addr, length, prot, flags, fd, offset);
+
+	if (ret < 0) {
+		errno = -ret;
+		return MAP_FAILED;
+	}
+
+	return (void *)ret;
 }
 
 void *mremap(void *old_address, size_t old_size, size_t new_size, int flags)
 {
 	/* TODO: Implement mremap(). */
-	return MAP_FAILED;
+	void *new_address = (void *)syscall(__NR_mremap, old_address, old_size, new_size, flags);
+	return new_address;
 }
 
 int munmap(void *addr, size_t length)
 {
 	/* TODO: Implement munmap(). */
-	return -1;
+	int ret = syscall(__NR_munmap, addr, length);
+
+	if (ret < 0) {
+		errno = EINVAL;
+		return -1;
+	}
+
+	return 0;
 }
